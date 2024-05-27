3b9abe789277e2e7adf73eb7e36b80d676cdc90d
feras2624 <rxm@DESKTOP-H10O83V.localdomain>
2024-05-27 15:47:51 +0300

    max

--- Logo/LogoGen/views.py
+++ Logo/LogoGen/views.py
@@ -14,7 +14,7 @@ def GenLogo(request):
 		name = request.data["name"]
 		keywords = request.data["desc"]
 		color = request.data["color"] 
-		number = int(request.data["number"]) if int(request.data["number"])<=25 else 25
+		number = int(request.data["number"])
 		q="businessDescription="+urllib.parse.quote_plus(businessDescription)+"&FilterByTags=&Colors="+urllib.parse.quote_plus(color)+"&text="+urllib.parse.quote_plus(name)+"&searchText="+urllib.parse.quote_plus(keywords.replace(' ',','))+"&customPrompt=&isFromAILogoGenerator=true"
 		url=baseurl+q
 		response = requests.get(url)
