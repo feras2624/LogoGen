30dda621329c66bd3be85dbfa2cf56ddcfaf4f65
feras2624 <rxm@DESKTOP-H10O83V.localdomain>
2024-05-28 13:44:32 +0300

    ll

--- Logo/Logo/settings.py
+++ Logo/Logo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'Logo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Logo',
+        'USER': 'root',
+        'PASSWORD': 'toor',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
 
@@ -118,7 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'Logo','LogoGen')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- Logo/LogoGen/models.py
+++ Logo/LogoGen/models.py
@@ -3,7 +3,9 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Logo(models.Model):
 	name = models.CharField(max_length=64)
 	desc = models.CharField(max_length=256)
+	img = models.CharField(max_length=50000,default='')
 	color = models.CharField(max_length=32,default=" ")
+	taken = models.BooleanField(default=False)
 	#number = models.IntegerField(default=10,validators=[MaxValueValidator(25),MinValueValidator(10)])
 	#img = models.ImageField(upload_to='images/', default=None)
 		
